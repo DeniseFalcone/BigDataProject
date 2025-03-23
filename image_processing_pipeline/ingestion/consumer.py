c04b0243d0671b42e98833b1e70eba4acaf6866f
DeniseFalcone <93253264+DeniseFalcone@users.noreply.github.com>
2025-03-23 11:07:00 +0100

    Connection between Kafka and Mongodb
    Created connection between kafka and mongodb. Implemented consumer and producer to save metadata in mongodb and implemented script to simulate realtime stream of images.
    
    Co-Authored-By: Chiara Cintioni <83300887+chiara-cintioni@users.noreply.github.com>

--- image_processing_pipeline/ingestion/consumer.py
+++ image_processing_pipeline/ingestion/consumer.py
@@ -1,30 +1,83 @@
-import threading, logging, time
+import os
+import json
+import logging
 import multiprocessing
 from kafka import KafkaConsumer
-import os
+from pymongo import MongoClient
+
+# Load environment variables
+KAFKA_URL = os.getenv("KAFKA_URL")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
+MONGO_COLLECTION = os.getenv("MONGO_METADATA_COLLECTION")
+
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
+collection = db[MONGO_COLLECTION]
+
+print(f"🛠 Connecting to Kafka: {KAFKA_URL}")
 
 class Consumer(multiprocessing.Process):
+
     def __init__(self):
         multiprocessing.Process.__init__(self)
         self.stop_event = multiprocessing.Event()
-        
+
     def stop(self):
         self.stop_event.set()
-        
+
+    def run33333(self):
+        consume_messages()
+
     def run(self):
-        consumer = KafkaConsumer(bootstrap_servers=os.getenv("KAFKA_BROKER"),
-                                 auto_offset_reset='earliest',
-                                 consumer_timeout_ms=1000)
-        consumer.subscribe(['my-topic'])
-        print("Consumer is going to wait for messages...")
+        consumer = KafkaConsumer(
+            KAFKA_TOPIC,
+            bootstrap_servers=KAFKA_URL,
+            value_deserializer=lambda v: json.loads(v.decode("utf-8")),
+            auto_offset_reset="earliest",
+            group_id="insert-metadata-mongo-group",
+            enable_auto_commit=True
+        )
+        for message in consumer:
+            logging.info(f"Received message: {message.value}")
+            dict = {"message": message.value}
+            collection.insert_one(dict)
+            logging.info(f"Saved to MongoDB: {message.value}")
+        
 
-        while not self.stop_event.is_set():
-            for message in consumer:
-                print("........Receiving data")
-                print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                        message.offset, message.key,
-                                        message.value))
-                if self.stop_event.is_set():
-                    break
 
+def consume_messages():
+
+    # Set up Kafka Consumer
+    consumer = KafkaConsumer(
+        KAFKA_TOPIC,
+        bootstrap_servers=KAFKA_URL,
+        value_deserializer=lambda v: json.loads(v.decode("utf-8")),
+        auto_offset_reset="earliest",
+        group_id="insert-metadata-mongo-group",
+        enable_auto_commit=True
+    )
+
+    
+    print("✅ Successfully connected to Kafka broker!")
+
+
+    logging.info(f"Connected to Kafka: {KAFKA_URL}, Topic: {KAFKA_TOPIC}")
+    logging.info(f"Connected to MongoDB: {MONGO_URI}, Database: {MONGO_DB}, Collection: {MONGO_COLLECTION}")
+
+    try:
+        for message in consumer:
+            metadata = message.value
+            logging.info(f"Received message: {metadata}")
+
+            # Save to MongoDB
+            collection.insert_one(metadata)
+            logging.info(f"Saved to MongoDB: {metadata}")
+
+    except Exception as e:
+        logging.error(f"Error: {e}")
+    finally:
         consumer.close()
+
--- image_processing_pipeline/ingestion/main.py
+++ image_processing_pipeline/ingestion/main.py
@@ -1,28 +1,26 @@
-import threading, logging, time
-import multiprocessing
-import consumer, producer
+import logging, time
+import consumer, producer, fake_realtime_stream
         
 def main():
     tasks = [
         consumer.Consumer(),
-        producer.Producer()
+        producer.Producer(),
+        fake_realtime_stream.RealTimeDataSimulator()
     ]
 
     for t in tasks:
         t.start()
 
-    time.sleep(10)
-    
-    for task in tasks:
-        task.stop()
+    time.sleep(10)  # Let the threads run
 
-    for task in tasks:
-        task.join()
+    for t in tasks:
+        t.join()  # Ensure threads complete execution
         
         
 if __name__ == "__main__":
     logging.basicConfig(
         format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
         level=logging.INFO
-        )
+    )
     main()
+
